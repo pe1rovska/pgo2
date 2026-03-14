34d850b1b9e38e78e0aa9a24993341fb8b4b6662
Anna <s35093@pjwstk.edu.pl>
2026-03-14 19:39:16 +0100

    Add helper information about the application

--- pgo-git-start/src/Main.java
+++ pgo-git-start/src/Main.java
@@ -1,1 +1,14 @@
-public class Main {} 
+package src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PGO Git Exercise Started");
+        showAppInfo();
+    }
+
+    private static void showAppInfo() {
+        System.out.println("Application: pgo-git-start");
+        System.out.println("Status: Learning Git Workflow");
+        System.out.println("Author: [Твое Имя]");
+    }
+}
